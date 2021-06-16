9eb64142dc8edb0f38e33a10daff552aab373568
Merey <merey.temirkas@bcc.kz>
2021-06-16 15:41:31 +0500

    NGRX-4 added effects for date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import {Component} from '@angular/core';
+import {Store} from "@ngrx/store";
+import {countSelector, decrease, increase, reset, updatedAtSelector} from "./reducers/counter";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -6,26 +9,23 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngRx-beginning';
-  counter = 0;
-  updatedAt?: number;
+  count$ = this.store.select(countSelector);
+  cannotDecrease$ = this.count$.pipe(map(count => count <= 0));
+  updatedAt$ = this.store.select(updatedAtSelector)
 
-  get cannotDecrease(): boolean {
-    return this.counter <= 0;
+  constructor(private store: Store) {
   }
 
+
   increase(): void {
-    this.updatedAt = Date.now();
-    this.counter++
+    this.store.dispatch(increase())
   }
 
   decrease(): void {
-    this.updatedAt = Date.now();
-    this.counter--
+    this.store.dispatch(decrease())
   }
 
   reset(): void {
-    this.updatedAt = Date.now();
-    this.counter = 0;
+    this.store.dispatch(reset())
   }
 }
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,18 +1,13 @@
-import {
-  ActionReducer,
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
-  MetaReducer
-} from '@ngrx/store';
-import { environment } from '../../environments/environment';
+import {ActionReducerMap, MetaReducer} from '@ngrx/store';
+import {environment} from '../../environments/environment';
+import {COUNTER_KEY, counterReducer, CounterState} from "./counter";
 
 export interface State {
-
+  [COUNTER_KEY]: CounterState
 }
 
 export const reducers: ActionReducerMap<State> = {
-
+  [COUNTER_KEY]: counterReducer
 };
 
 
